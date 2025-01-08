ad44ccb29e74b068305a571bd655ff82a24fbe37
Sinan1985 <32837145+Sinan1985@users.noreply.github.com>
2025-01-08 10:40:43 +0100

    Update settings.py

--- karaoke/settings.py
+++ karaoke/settings.py
@@ -135,6 +135,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 django_heroku.settings(locals())
